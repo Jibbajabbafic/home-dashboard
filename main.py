bcd28af759b86214e00bf95785be95661ed01e5e
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2025-09-29 13:26:50 +0100

    feat: make flask page to display times

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import urllib.request
 from html.parser import HTMLParser
+from flask import Flask, render_template_string
 
 
 class TramTimeParser(HTMLParser):
@@ -39,7 +40,7 @@ class TramTimeParser(HTMLParser):
                     self.tram_times.append(time)
 
 
-def main():
+def get_tram_times():
     url = "https://connect.wyca.vix-its.com/Text/WebDisplay.aspx?stopRef=9400ZZSYMID1&stopName=Middlewood+To+City"
 
     req = urllib.request.Request(url)
@@ -48,11 +49,40 @@ def main():
 
     parser = TramTimeParser()
     parser.feed(html_content)
+    return parser.tram_times
 
-    print("Next tram times:")
-    for time in parser.tram_times:
-        print(time)
+
+app = Flask(__name__)
+
+HTML_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tram Times</title>
+    <style>
+        body { font-family: sans-serif; }
+        h1 { color: #333; }
+        ul { list-style-type: none; padding: 0; }
+        li { background: #f4f4f4; margin: 5px 0; padding: 10px; border-radius: 5px; }
+    </style>
+</head>
+<body>
+    <h1>Next Tram Times</h1>
+    <ul>
+        {% for time in times %}
+            <li>{{ time }}</li>
+        {% endfor %}
+    </ul>
+</body>
+</html>
+"""
+
+
+@app.route("/")
+def index():
+    times = get_tram_times()
+    return render_template_string(HTML_TEMPLATE, times=times)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=3000)
