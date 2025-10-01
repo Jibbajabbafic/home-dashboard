177d5468d327ae4bfaa3bc09496b2fb1210d80c5
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2025-10-01 23:58:32 +0100

    fix: build script

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker login
-if docker buildx ls | grep -q desktop-linux; then
+if docker buildx inspect | grep -q "Endpoint:.*unix:///var/run/docker.sock" ; then
     echo "Builder exists already"
 else
     echo "Builder does not exist - creating new one"
-    docker buildx create --use desktop-linux
+    docker buildx create --use default
 fi
 docker buildx build --platform linux/amd64,linux/arm64 -t jibby/home-dashboard:latest --push .
